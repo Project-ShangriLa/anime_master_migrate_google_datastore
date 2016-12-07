0f3e7a2bf7d269d5c1afd9814795c1fdbb2b6f3d
AKB428 <gandara016+1@gmail.com>
2016-12-08 06:39:08 +0900

    fix time

--- migrate.rb
+++ migrate.rb
@@ -29,13 +29,12 @@ master_list.each do |master|
 
   #"2016-09-19 19:24:09.0" -> "2016-09-19T00:00:00+09:00"
   # Google側のTime型はRCF3339
-  master['created_at'] = Date.parse(master['created_at'], "%Y-%m-%d %HH:%MM:%SS").to_time.to_datetime.to_s
-  master['updated_at'] = Date.parse(master['updated_at'], "%Y-%m-%d %HH:%MM:%SS").to_time.to_datetime.to_s
+  master['created_at'] = DateTime.parse(master['created_at'], "%Y-%m-%d %HH:%MM:%SS").to_time.to_datetime.to_s
+  master['updated_at'] = DateTime.parse(master['updated_at'], "%Y-%m-%d %HH:%MM:%SS").to_time.to_datetime.to_s
 
   json = JSON.dump(master)
   http_client = HTTPClient.new
   url = URI.parse(URI.encode( File.join('http://' + gae_host, 'anime/v1/master') ))
   response = http_client.put(url, json, auth_header)
   puts response.status
-
 end
